04bb2c5d7710ed7209fedbfd7422603feb487846
nowRick <alamajjkin@rambler.ru>
2020-10-05 21:18:25 +0300

    add proxy

--- src/app/city-list/city-list.component.ts
+++ src/app/city-list/city-list.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { WeatherService } from '../services/weather.service';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-city-list',
@@ -16,23 +16,18 @@ export class CityListComponent implements OnInit {
   constructor(private weatherService: WeatherService, private http: HttpClient) { }
   
   getWeather() {
-    const options = {
-      headers: new HttpHeaders({
-        'Access-Control-Allow-Origin': '*'
-      })
-    }
-
-    this.http.get('https://www.metaweather.com/api/location/2122265/')
+    this.http.get('api/location/2122265/')
     .subscribe((response) => {
       this.response = response;
       console.log(response);
+      console.log(response['consolidated_weather']);
+      
     });
   }
 
   ngOnInit(): void {
     this.cities = this.weatherService.getCities();
   }
-  
   
   // @Output() cityId = new EventEmitter<string>();
   
